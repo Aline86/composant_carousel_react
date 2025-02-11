0b42b8cf32ee90f683aaac3e7d805172c4738e03
Claire-Aline Haestie <ca.haestie@gmail.com>
2025-02-11 18:04:04 +0100

    ok for miniatures like on other website with swipe effect

--- src/components/CarouselContainer.tsx
+++ src/components/CarouselContainer.tsx
@@ -161,7 +161,7 @@ function CarouselContainer({
             minWidth: `${cardWidth + gap} px`,
             margin: `${gap}px auto`,
             height: `${!result.matches ? height + 2 : height * 0.3 + 3}px`,
-            width: `${result.matches ? "60vw" : "90vw"}`,
+            width: `${result.matches ? "80vw" : "90vw"}`,
             maxWidth: "100%",
           }}
         >
